23e0c0f03229c588118966e3f8b40527c0349a48
trev-slack <61800073+trev-slack@users.noreply.github.com>
2020-05-12 15:56:57 -0600

    Add files via upload

--- GUI_test.py
+++ GUI_test.py
@@ -80,19 +80,54 @@ class myWidget( QWidget ):
         pushButtonRight.setText("Right")
         pushButtonRight.setAutoRepeat(True)
 
-        control_box.addWidget(QtWidgets.QPushButton('1'),0,0)
+        #control_box.addWidget(QtWidgets.QPushButton('1'),0,0)
         control_box.addWidget(pushButtonForwards,0,1)
-        control_box.addWidget(QtWidgets.QPushButton('3'),0,2)
+        #control_box.addWidget(QtWidgets.QPushButton('3'),0,2)
         control_box.addWidget(pushButtonLeft,1,0)
-        control_box.addWidget(QtWidgets.QPushButton('5'),1,1)
+        #control_box.addWidget(QtWidgets.QPushButton('5'),1,1)
         control_box.addWidget(pushButtonRight,1,2)
-        control_box.addWidget(QtWidgets.QPushButton('7'),2,0)
+        #control_box.addWidget(QtWidgets.QPushButton('7'),2,0)
         control_box.addWidget(pushButtonBackwards,2,1)
-        control_box.addWidget(QtWidgets.QPushButton('9'),2,2)        
+        #control_box.addWidget(QtWidgets.QPushButton('9'),2,2)        
 
-        main_layout = QHBoxLayout()
-        main_layout.addLayout(rviz_layout)
-        main_layout.addLayout(control_box)
+        speed_box = QGridLayout()
+
+        self.qdialspeed = QtWidgets.QDial(self)
+        self.qdialspeed.setObjectName("QDialSpeed")
+        self.qdialspeed.setMinimum(1)
+        self.qdialspeed.setMaximum(10)
+        self.qdialspeed.setValue(1)
+        self.qdialspeed.setNotchesVisible(True)
+        speed_box.addWidget(self.qdialspeed,0,0)
+
+        self.qdialAngspeed = QtWidgets.QDial(self)
+        self.qdialAngspeed.setObjectName("QDialAngSpeed")
+        self.qdialAngspeed.setMinimum(1)
+        self.qdialAngspeed.setMaximum(10)
+        self.qdialAngspeed.setValue(1)
+        self.qdialAngspeed.setNotchesVisible(True)
+        speed_box.addWidget(self.qdialAngspeed,0,1)
+
+        self.labelVel = QtWidgets.QLabel(self)
+        self.labelVel.setObjectName("labelVel")
+        self.labelVel.setText("Linear Speed : 1")
+        self.labelVel.adjustSize()
+        speed_box.addWidget(self.labelVel,1,0)
+
+        self.labelAngVel = QtWidgets.QLabel(self)
+        self.labelAngVel.setObjectName("labelAngVel")
+        self.labelAngVel.setText("Angular Speed : 1")
+        self.labelAngVel.adjustSize()
+        speed_box.addWidget(self.labelAngVel,1,1)
+
+
+
+        main_layout = QGridLayout()
+        main_layout.addLayout(rviz_layout,0,0,0,1)
+        main_layout.addLayout(control_box,0,1)
+        main_layout.addLayout(speed_box,1,1)
+
+
 
 
         self.setLayout( main_layout )
@@ -103,6 +138,15 @@ class myWidget( QWidget ):
         pushButtonBackwards.pressed.connect(self.moveBackwards)
         pushButtonLeft.pressed.connect(self.moveLeft)
         pushButtonRight.pressed.connect(self.moveRight)
+        self.qdialspeed.sliderReleased.connect(self.updateLin)
+        self.qdialAngspeed.sliderReleased.connect(self.updateAng)
+
+    def updateLabel(self):
+        self.labelVel.setText("Linear Speed : " + str(self.qdialspeed.value()))
+        self.labelVel.adjustSize()
+    def updateLabelAng(self):
+        self.labelAngVel.setText("Angular Speed : " + str(self.qdialAngspeed.value()))
+        self.labelAngVel.adjustSize()
 
 
     def moveForward(self):
@@ -144,6 +188,7 @@ class myWidget( QWidget ):
                 view_man.setCurrentFrom( view_man.getViewAt( i ))
                 return
         print( "Did not find view named %s." % view_name )
+
 
 if __name__ == '__main__':
     try:
